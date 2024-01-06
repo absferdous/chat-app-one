4ff927407736ae33e5db3607135089c4fe5ed61c
raiyanmrf <raiyanmrf@gmail.com>
2024-01-07 00:23:28 +0600

    added styles in signout

--- src/components/SignOut.jsx
+++ src/components/SignOut.jsx
@@ -7,12 +7,12 @@ import Buttons from "./Buttons";
 const SignOut = ({ signout }) => {
   return (
     <>
-      <div className='  border-2  border-cornflower-400  bg-gradient-to-r from-cornflower-500 to-cornflower-900 relative z-10 rounded  px-3 mt-1 flex justify-between items-center gap-6  mx-1'>
+      <div className='  border-cornflower-400  bg-gradient-to-r from-cornflower-500 to-cornflower-900 relative z-10 rounded  px-3 mt-1 flex justify-between items-center gap-6  mx-1'>
         {auth.currentUser && (
           <img
             className=' rounded-full'
-            width={25}
-            height={25}
+            width={35}
+            height={35}
             src={auth.currentUser.photoURL}
             alt={auth.currentUser.displayName}
           />
